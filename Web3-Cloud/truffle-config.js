d45810c960594936355d1df401cca94a2199e6ef
Tirtha415 <tirtharajmukherjee1@gmail.com>
2022-06-05 18:23:42 +0530

    installed all the dependencies

--- Web3-Cloud/truffle-config.js
+++ Web3-Cloud/truffle-config.js
@@ -60,13 +60,6 @@ module.exports = {
     }
   },
 
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
   // After you backed up your artifacts you can utilize db by running migrate as follows:
   // $ truffle migrate --reset --compile-all
   //
